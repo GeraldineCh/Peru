05e550a7d7fbe239e910b72942d036b6f12a9893
Betsi Loayza <betsi18_12@hotmail.com>
2017-04-25 17:09:21 -0500

    estructura de imagenes

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ function carrusel() {
     var i;
     var x = document.getElementsByClassName("portada");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
+      x[i].style.display = "none";
     }
     indice++;
-    if (indice > x.length) {indice = 1} 
-    x[indice-1].style.display = "block"; 
+    if (indice > x.length) {indice = 1}
+    x[indice-1].style.display = "block";
     setTimeout(carrusel, 5000); // Change image every 2 seconds
 }
 carrusel(); //Callback Carrousel
@@ -32,4 +32,3 @@ function detener() {
     iniciar();
   })
 });*/
-
